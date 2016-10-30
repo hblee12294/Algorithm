dfe7a705a47dcaec61ac7e141cb9ffbdd54c77f2
hblee12294 <hblee12294@gmail.com>
2016-10-30 23:32:13 +0800

    create List

---
+++ List/List.h
@@ -0,0 +1,14 @@
+#define Posi(T) ListNode<T>*
+
+template <typename T>
+struct ListNode
+{
+	T data;
+	Posi(T) pred;
+	Posi(T) succ;
+	ListNode() {}
+	ListNode(T e, Posi(T) p = NULL, Posi(T) s = NULL) :
+		data(e), pred(p), succ(s) {}
+	Posi(T) insertAsPred(T const &e);
+	Posi(T) insertAsSucc(T const &e);
+};
---
+++ List/clear.cpp
@@ -0,0 +1,16 @@
+template <typename T>
+int List<T>::clear()
+{
+	int oldSize = _size;
+	while (0 < _size)
+		remove(header->succ);
+	return oldSize;
+}
+
+template <typename T>
+List<T>::~List()
+{
+	clear();
+	delete header;
+	delete trailer;
+}
